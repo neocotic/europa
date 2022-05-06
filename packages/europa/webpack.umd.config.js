0b01448731bd39c274245a9967a1f3bf976476bf
Alasdair <alasdair.mercer@unity3d.com>
2022-05-06 01:12:52 +0100

    Remove copyright header from build files

--- packages/europa/webpack.umd.config.js
+++ packages/europa/webpack.umd.config.js
@@ -1,25 +1,3 @@
-/*
- * Copyright (C) 2022 neocotic
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
@@ -71,9 +49,7 @@ module.exports = {
   },
   plugins: [
     new BannerPlugin({
-      banner: `Europa v${version} | (C) ${new Date().getFullYear()} ${
-        author.name
-      } | ${license} License`,
+      banner: `Europa v${version} | (C) ${new Date().getFullYear()} ${author.name} | ${license} License`,
     }),
   ],
   resolve: {
